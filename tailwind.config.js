e7083ff785a6bfc3c12e225d4e498f4d4e59f6f0
FlaceyFurr <flaceyfurr@gmail.com>
2023-03-04 10:37:22 +0200

    Added colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
     fontFamily: {
       ubuntu: ["Ubuntu", "sans-serif"],
     },
+    colors: {
+      marineBlue: "hsl(354, 84%, 57%)",
+      purplishBlue: "hsl(354, 84%, 57%)",
+      pastelBlue: "hsl(228, 100%, 84%)",
+      lightBlue: "hsl(206, 94%, 87%)",
+      strawberryRed: "hsl(354, 84%, 57%)",
+      coolGray: "hsl(231, 11%, 63%)",
+      lightGray: "hsl(229, 24%, 87%)",
+      magnolia: "hsl(229, 24%, 87%)",
+      alabaster: "hsl(231, 100%, 99%)",
+      white: "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
